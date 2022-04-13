30083e33d5045818d047bf27272b14c443f8c5dc
Артур Токранов <artur.tokranov@gmail.com>
2022-04-13 20:41:34 +0300

    corrected project

--- file_downloader/app_downloader/models.py
+++ file_downloader/app_downloader/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class File(models.Model):
     name = models.CharField(max_length=140, verbose_name='Название файла')
     file = models.FileField(
-        upload_to='Files/', verbose_name='Файл')
+        upload_to='Files/', verbose_name='Файлы')
     size = models.IntegerField(
         default=0, editable=False, verbose_name='Размер')
+    file_extensions = models.CharField(
+        max_length=17, verbose_name='Расширение файла')
     time_create = models.DateTimeField(
         auto_now_add=True, verbose_name='Дата загрузки')
 
--- file_downloader/app_downloader/serializers.py
+++ file_downloader/app_downloader/serializers.py
@@ -5,9 +5,14 @@ from .models import File
 class FilesSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
-        fields = ('id', 'name', 'file', 'size', 'time_create')
-        read_only_fields = ('id', 'size',)
+        fields = (
+            'id', 'name', 'file', 'size', 'file_extensions', 'time_create'
+        )
+        read_only_fields = ('id', 'size', 'file_extensions')
 
     def create(self, validated_data):
         return File.objects.create(
-            **validated_data, size=validated_data['file'].size)
+            **validated_data,
+            size=validated_data['file'].size,
+            file_extensions=validated_data['file'].name.split('.')[-1]
+        )
